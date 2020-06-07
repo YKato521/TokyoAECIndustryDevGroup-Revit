372c1f1e73c2bfbf69a874563502a0960364b11f
YKato521 <61691395+YKato521@users.noreply.github.com>
2020-06-07 17:02:10 +0900

    Sample1

---
+++ TokyoAECDev.extension/TokyoAECDev.tab/Sheet.panel/Sample1.pushbutton/script.py
@@ -0,0 +1,56 @@
+from Autodesk.Revit import DB
+from rpw import db
+
+"""
+######################
+## Code Sample (C#) ##
+######################
+
+UIApplication uiapp = commandData.Application;
+UIDocument uidoc = uiapp.ActiveUIDocument;
+Application app = uiapp.Application;
+Document doc = uidoc.Document;
+
+python
+commandData.Application => __revit__
+
+"""
+
+# C#
+"""
+uiapp = __revit__
+uidoc = uiapp.ActiveUIDocument
+app = uiapp.Application
+doc = uidoc.Document
+
+sheets = (
+    DB.FilteredElementCollector(doc)
+    .OfClass(DB.ViewSheet)
+    .WhereElementIsNotElementType()
+    .ToElements())
+for sheet in sheets:
+    print(sheet)
+    print(sheet.Name)
+"""
+
+# RPW UnwrappedElement
+"""
+sheets = db.Collector(of_class='ViewSheet', is_not_type=True)
+for sheet in sheets:
+    print(sheet)
+    print(sheet.Name)
+"""
+# RPW WrappedElement
+
+sheets = db.Collector(of_class='ViewSheet', is_not_type=True).get_elements()
+for sheet in sheets:
+    print(sheet)
+    print(sheet.name)
+
+# RPW db.ViewSheet
+"""
+sheets = db.ViewSheet.collect()
+for sheet in sheets:
+    print(sheet)
+    print(sheet.Name)
+"""
