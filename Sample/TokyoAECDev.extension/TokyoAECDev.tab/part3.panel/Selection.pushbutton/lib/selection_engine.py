ed89959032bc4246d3ced90786d67e5fc5956d12
YKato521 <61691395+YKato521@users.noreply.github.com>
2020-10-24 16:58:40 +0900

    updated

--- Sample/TokyoAECDev.extension/TokyoAECDev.tab/part3.panel/Selection.pushbutton/lib/selection_engine.py
+++ Sample/TokyoAECDev.extension/TokyoAECDev.tab/part3.panel/Selection.pushbutton/lib/selection_engine.py
@@ -32,35 +32,41 @@ class selectionEngine(object):
     """
 
     # Part2
+    """
     def select_by_name(self):
         select_dict = {}
-        select_list = []
         selection = Selection()
         els = selection.get()
         for el in els:
             if el.Symbol.FamilyName not in select_dict.keys():
-                select_dict[el.Symbol.FamilyName] = el
+                select_dict[el.Symbol.FamilyName] = [el.Id]
+            else:
+                select_dict[el.Symbol.FamilyName].append(el.Id)
         form = SelectFromList("Select By FamilyName", select_dict, exit_on_close=True)
-        for el in els:
-            if el.Symbol.Id == form.Symbol.Id:
-                select_list.append(el.Id)      
-        selection.set(select_list)
+        if form is not None:
+            selection.set(form)
     
     def select_by_param(self):
+        select_dict = {}
         param_list = []
         selection = Selection()
         els = selection.get()
         for el in els:
             param = _(el).get('Comments')
-            try:
-                par = el.get_Parameter(BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS).AsString()
-            except:
-                par = None
-            # print(param, par)
-        for el in els:
-            if el.Symbol.FamilyName not in select_dict.keys():
-                select_dict[el.Symbol.FamilyName] = el
-        form = SelectFromList("Select By FamilyName", select_dict, exit_on_close=True)
-            if 'change' in param.lower():
-                param_list.append(el.Id)
-        selection.set(param_list)
+            
+            #try:
+            #    param = el.get_Parameter(BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS).AsString()
+            #except:
+            #    param = None
+            
+            if param not in select_dict.keys():
+                select_dict[param] = [el.Id]
+            else:
+                if select_dict[param]:
+                    select_dict[param].append(el.Id)
+                else:
+                    select_dict[param] = [el.Id]
+        form = SelectFromList("Select By Comments", select_dict, exit_on_close=True)
+        if form is not None:
+            selection.set(form)
+    """
---
+++ TokyoAECDev.extension/TokyoAECDev.tab/part3.panel/Selection.pushbutton/lib/selection_engine.py
@@ -0,0 +1,118 @@
+from Autodesk.Revit.DB import BuiltInParameter, FamilyInstance
+from revitron import Selection, DOC, _
+from rpw.ui.forms import SelectFromList
+
+
+class selectionEngine(object):
+    # Part1
+    """
+    def select_by_name(self):
+        table_list = []
+        selection = Selection()
+        els = selection.get()
+        for el in els:
+            if 'table' in el.Symbol.FamilyName.lower():
+                table_list.append(el.Id)
+        selection.set(table_list)
+    
+    def select_by_param(self):
+        param_list = []
+        selection = Selection()
+        els = selection.get()
+        for el in els:
+            param = _(el).get('Comments')
+            try:
+                par = el.get_Parameter(BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS).AsString()
+            except:
+                par = None
+            # print(param, par)
+            if 'change' in param.lower():
+                param_list.append(el.Id)
+        selection.set(param_list)
+    """
+
+    # Part2
+    """
+    def select_by_name(self):
+        select_dict = {}
+        selection = Selection()
+        els = selection.get()
+        for el in els:
+            if el.Symbol.FamilyName not in select_dict.keys():
+                select_dict[el.Symbol.FamilyName] = [el.Id]
+            else:
+                select_dict[el.Symbol.FamilyName].append(el.Id)
+        form = SelectFromList("Select By FamilyName", select_dict, exit_on_close=True)
+        if form is not None:
+            selection.set(form)
+    
+    def select_by_param(self):
+        select_dict = {}
+        param_list = []
+        selection = Selection()
+        els = selection.get()
+        for el in els:
+            param = _(el).get('Comments')
+            
+            #try:
+            #    param = el.get_Parameter(BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS).AsString()
+            #except:
+            #    param = None
+            
+            if param not in select_dict.keys():
+                select_dict[param] = [el.Id]
+            else:
+                if select_dict[param]:
+                    select_dict[param].append(el.Id)
+                else:
+                    select_dict[param] = [el.Id]
+        form = SelectFromList("Select By Comments", select_dict, exit_on_close=True)
+        if form is not None:
+            selection.set(form)
+    """
+    # Part3
+    def select_by_name(self):
+        select_dict = {}
+        selection = Selection()
+        els = selection.get()
+        for el in els:
+            if isinstance(el, FamilyInstance):
+                if el.Symbol.FamilyName not in select_dict.keys():
+                    select_dict[el.Symbol.FamilyName] = [el.Id]
+                else:
+                    select_dict[el.Symbol.FamilyName].append(el.Id)
+            elif DOC.GetElement(el.GetTypeId()):
+                el_name = DOC.GetElement(el.GetTypeId()).FamilyName
+                if el_name not in select_dict.keys():
+                    select_dict[el_name] = [el.Id]
+                else:
+                    select_dict[el_name].append(el.Id)
+
+        form = SelectFromList("Select By FamilyName", select_dict, exit_on_close=True)
+        if form is not None:
+            selection.set(form)
+    
+    def select_by_param(self):
+        select_dict = {}
+        param_list = []
+        selection = Selection()
+        els = selection.get()
+        for el in els:
+            param = _(el).get('Comments')
+            
+            #try:
+            #    param = el.get_Parameter(BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS).AsString()
+            #except:
+            #    param = None
+            
+            if param not in select_dict.keys():
+                select_dict[param] = [el.Id]
+            else:
+                if select_dict[param]:
+                    select_dict[param].append(el.Id)
+                else:
+                    select_dict[param] = [el.Id]
+        form = SelectFromList("Select By Comments", select_dict, exit_on_close=True)
+        if form is not None:
+            selection.set(form)
+
